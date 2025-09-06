0fbac219008f1b1015adb2584571ffab030dd122
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-06 22:24:08 +0600

    Implement rudimentary progressbar

--- main.py
+++ main.py
@@ -3,28 +3,33 @@ from OpenGL.GLUT import *
 import sys
 
 PALETTE = {
-    'crashblue': (0.035, 0.031, 0.549)
+    'crashblue': (0.035, 0.031, 0.549),
+    'white': (1, 1, 1)
 }
 
 def getColor(k):
     return PALETTE.get(k, (1, 1, 1))
 
 def display():
+    global bar
     glClear(GL_COLOR_BUFFER_BIT)
+    bar.draw()
     glutSwapBuffers()
 
 def animate():
     glutPostRedisplay()
 
 def keybinds(key, x, y):
+    global bar
+    step = 1
     if key == b"w":
         pass
     if key == b"s":
         pass
     if key == b"a":
-        pass
+        bar.update(-step)
     if key == b"d":
-        pass
+        bar.update(step)
 
 def getTitle():
     if len(sys.argv) == 2:
@@ -33,9 +38,50 @@ def getTitle():
         title = b"Progressbar"
     return title
 
+class Bar:
+    def __init__(self, p=0):
+        global win_w, win_h
+        self.progress = p
+        self.x = win_w/2
+        self.y = win_h/2
+    def update(self, v):
+        self.progress += v
+        self.clamp()
+    def clamp(self):
+        if self.progress > 100:
+            self.progress = 100
+        elif self.progress < 0:
+            self.progress = 0
+
+    def draw(self):
+        height = 20
+        width = 200
+        filled = width * (self.progress/100)
+
+        glBegin(GL_QUADS)
+        glColor3f(*getColor('white'))
+        glVertex2f(self.x - width/2, self.y - height/2)
+        glVertex2f(self.x - width/2 + filled, self.y - height/2)
+        glVertex2f(self.x - width/2 + filled, self.y + height/2)
+        glVertex2f(self.x - width/2, self.y + height/2)
+        glEnd()
+
+        glBegin(GL_LINES)
+        glColor3f(*getColor('white'))
+        glVertex2f(self.x - width/2, self.y - height/2)
+        glVertex2f(self.x + width/2, self.y - height/2)
+        glVertex2f(self.x + width/2, self.y - height/2)
+        glVertex2f(self.x + width/2, self.y + height/2)
+        glVertex2f(self.x + width/2, self.y + height/2)
+        glVertex2f(self.x - width/2, self.y + height/2)
+        glVertex2f(self.x - width/2, self.y + height/2)
+        glVertex2f(self.x - width/2, self.y - height/2)
+        glEnd()
+
 def main(title):
-    global win_w, win_h
+    global win_w, win_h, bar
     win_w, win_h = 300, 200
+    bar = Bar()
     glutInit()
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
     glutInitWindowPosition(0, 0)
